3b6abf5656b372da7878eda5d8dd594be70bbea4
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-15 14:17:39 -0300

    12° commit - tdd controller: Consegue listar uma language especifica e retornar status 200? RED

--- spec/controllers/v1/languages_controller_v1_spec.rb
+++ spec/controllers/v1/languages_controller_v1_spec.rb
@@ -5,9 +5,17 @@ RSpec.describe Api::V1::LanguagesController, type: :controller do
   before {@language = Language.create(name: "php", framework: "laravel")}
 
   describe "GET /api/v1/languages" do
-    it "Consegue criar uma language e retornar status 200?" do
+    it "Consegue listar todas as languages e retornar status 200?" do
       get :index
       expect(JSON.parse(response.body).size).to eq(1)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /api/v1/languages/id" do
+    it "Consegue listar uma language especifica e retornar status 200?" do
+      get :show, params: {id: @language.id}
+      expect(response.body).to include_json(id: @language.id)
       expect(response).to have_http_status(200)
     end
   end
