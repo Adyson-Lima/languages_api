0c469cd34a3015e113b178eb20bced05aa70bceb
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-15 14:43:43 -0300

    19° commit - tdd controller: Consegue excluir uma language e retornar status 204? GREEN

--- app/controllers/api/v1/languages_controller.rb
+++ app/controllers/api/v1/languages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LanguagesController < ApplicationController
 
-  before_action :set_language, only: %i[show update] #show update destroy
+  before_action :set_language, only: %i[show update destroy] #show update destroy
 
   def index
     @languages = Language.all
@@ -26,6 +26,10 @@ class Api::V1::LanguagesController < ApplicationController
     else
       render json: @language.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @language.destroy!
   end
 
 private
