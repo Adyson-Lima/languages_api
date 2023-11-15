6f1f5411c6b06177ab9678acccd0c68614c505d7
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-15 14:38:15 -0300

    17° coomit - tdd controller: Consegue atualizar uma language e retornar staus 200? GREEN

--- app/controllers/api/v1/languages_controller.rb
+++ app/controllers/api/v1/languages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LanguagesController < ApplicationController
 
-  before_action :set_language, only: %i[show] #show update destroy
+  before_action :set_language, only: %i[show update] #show update destroy
 
   def index
     @languages = Language.all
@@ -15,6 +15,14 @@ class Api::V1::LanguagesController < ApplicationController
     @language = Language.new(language_params)
     if @language.save
       render json: @language, status: :created, location: api_v1_language_url(@language)
+    else
+      render json: @language.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @language.update(language_params)
+      render json: @language
     else
       render json: @language.errors, status: :unprocessable_entity
     end
